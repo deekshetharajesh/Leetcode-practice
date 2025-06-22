dea0373e048def134e128b4e8a5210b5d7d728b7
Deekshetha Rajesh <147843273+deekshetharajesh@users.noreply.github.com>
2025-06-22 21:50:06 +0530

    Circularqueue.java

---
+++ Circularqueue.java
@@ -0,0 +1,58 @@
+class MyCircularQueue {
+    private int[] queue;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+
+    public MyCircularQueue(int k) {
+        queue = new int[k];
+        front = -1;
+        rear = -1;
+        size = 0;
+        capacity = k;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        if (isEmpty()) {
+            front = 0;
+        }
+        rear = (rear + 1) % capacity;
+        queue[rear] = value;
+        size++;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        } else {
+            front = (front + 1) % capacity;
+        }
+        size--;
+        return true;
+    }
+
+    public int Front() {
+        return isEmpty() ? -1 : queue[front];
+    }
+
+    public int Rear() {
+        return isEmpty() ? -1 : queue[rear];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+}

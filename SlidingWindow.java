cef860433bf3d8eb37dca1f4a4bd6926d412e21e
Deekshetha Rajesh <147843273+deekshetharajesh@users.noreply.github.com>
2025-06-22 21:51:59 +0530

    SlidingWindow.java

---
+++ SlidingWindow.java
@@ -0,0 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || k <= 0) return new int[0];
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        int ri = 0;
+        
+        Deque<Integer> q = new ArrayDeque<>();
+        
+        for (int i = 0; i < n; i++) {
+            while (!q.isEmpty() && q.peek() < i - k + 1) {
+                q.poll();
+            }
+            while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) {
+                q.pollLast();
+            }
+            q.offer(i);
+            if (i >= k - 1) {
+                result[ri++] = nums[q.peek()];
+            }
+        }
+        
+        return result;
+    }
+}

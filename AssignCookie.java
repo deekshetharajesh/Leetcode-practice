7c9157914bc9f17f1e1d956ba28ca3f965ec403f
Deekshetha Rajesh <147843273+deekshetharajesh@users.noreply.github.com>
2025-06-22 22:16:18 +0530

    AssignCookie.java

---
+++ AssignCookie.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int contentChildren = 0;
+        int cookieIndex = 0;
+        int childIndex = 0;
+        while (childIndex < g.length && cookieIndex < s.length) {
+            if (s[cookieIndex] >= g[childIndex]) {
+                contentChildren++;
+                childIndex++;
+                cookieIndex++;
+            } else {
+                cookieIndex++;
+            }
+        }
+        
+        return contentChildren;
+    }
+}
